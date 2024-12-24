7ab920d8a7da44784747ade70895070384949888
Danilabv <161813147+BogolubDimiyan@users.noreply.github.com>
2024-12-24 09:54:30 +0300

    Api

--- ClientAp1/obj/Debug/net7.0/ClientAp1.AssemblyInfo.cs
+++ ClientAp1/obj/Debug/net7.0/ClientAp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientAp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27126418cc2c312870a82d4e31dd3ecf29e81d18")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientAp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientAp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
