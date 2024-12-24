b85450f1673339551c879fe35b5c107c9011344e
Danilabv <161813147+BogolubDimiyan@users.noreply.github.com>
2024-12-24 10:23:32 +0300

    Docker

--- src/BusinessLogic/obj/Debug/net7.0/BusinessLogic.AssemblyInfo.cs
+++ src/BusinessLogic/obj/Debug/net7.0/BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27126418cc2c312870a82d4e31dd3ecf29e81d18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e949104e88352f6d671bffad90cc4f9a4f902e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/ClientAp1/obj/Debug/net7.0/ClientAp1.AssemblyInfo.cs
+++ src/ClientAp1/obj/Debug/net7.0/ClientAp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientAp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3be224cb1bc93b78cd7bcfef9e187a9d6a50ce85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e949104e88352f6d671bffad90cc4f9a4f902e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientAp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientAp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
